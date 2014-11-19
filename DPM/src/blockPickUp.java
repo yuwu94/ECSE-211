97946979a6db9d52d5dfe4f69e6ad05a8ac0d8aa
yuwu94 <richard_wu94@hotmail.com>
2014-11-19 09:36:13 -0500

    blockPickUp Beta Version

--- DPM/src/blockPickUp.java
+++ DPM/src/blockPickUp.java
@@ -1,81 +1,94 @@
 import lejos.nxt.*;
-public class blockPickUp<UltrasonicController> {
+ 
+ 
+ 
+public class blockPickUp extends Thread {
         public static final int ROTATE_SPEED = 150;
         public static final int FORWARD_SPEED = 200;
-        public static final double forwardDistance = 5.0;
+        public static final double forwardDistance = 12.0;
+        public static final double forwardThreshold = 22.0;
         public final double leftRadius = 2.1;
         public final double rightRadius = 2.15;
         public final double width = 10.0;
-        private NXTRegulatedMotor claw;
+        private ClawDriver claw;
         private UltrasonicSensor sensor;
         private NXTRegulatedMotor leftMotor,rightMotor;
-        
-        private static UltrasonicPoller poller;
-        private  UltrasonicController usController;
-        
+       
+//        private static UltrasonicPoller poller;
+//        private  UltrasonicController usController;
+       
         //initiating objects
         WheelDriver wheels = new WheelDriver( leftMotor, rightMotor) ;
         Odometer odo = new Odometer();
         Navigation nav = new Navigation(leftRadius, rightRadius, odo, wheels);
-        ClawDriver theClaw = new ClawDriver(claw);
-        
+                UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
        
-       
-        public void findRange (NXTRegulatedMotor leftMotor, NXTRegulatedMotor rightMotor, double leftRadius, double rightRadius, double width) {
-                // reset the motors
-                for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { leftMotor, rightMotor }) {
-                        motor.stop();
-                        motor.setAcceleration(10000);
-                }              
-                // wait 5 seconds
-                try {
-                        Thread.sleep(2000);
-                }
-                catch (InterruptedException e) {
-                        // there is nothing to be done here because it is not expected that
-                        // the odometer will be interrupted by another thread
-                }
-                
-                boolean stopSearch = false;
-                
-                while (stopSearch){
-                                
-                                //make this loop forever while this function runs
-                                poller.getDist();                        
-                                
-                                //rotate between range of -45 and 45 degrees
-                                rotateCCW(45);
-                                rotateCCW(-90);
-                                if (poller.getDist() > 10) {
-                                        rotateCCW(45);
-                                        travelForward(forwardDistance);
+        public blockPickUp (ClawDriver claw, NXTRegulatedMotor leftMotor, NXTRegulatedMotor rightMotor ) {
+                this.claw = claw;
+                this.leftMotor = leftMotor;
+                this.rightMotor= rightMotor;
+        }
+ 
+        public void scanRange() {
+               
+                int threshold = 25;            
+//                  while (getFilteredData() > threshold) {
+//                          //rotate between range of -45 and 45 degrees
+//                          Sound.buzz();      
+//                              LCD.clear();
+//                      LCD.drawString("Dis:" + us.getDistance(), 0 , 0);
+//                      travelForward(forwardDistance);
+//                  }
+                boolean travel = true;
+                double turnAngle = -20;
+                while (travel){
+                        rotateCCW(60);
+                        if (us.getDistance() < threshold) {
+                                travelForward(forwardThreshold);
+                                claw.close();
+                                break;
+                        }
+                       
+                        for (int i = 0 ; i < 7 ; i++){
+                                rotateCCW(turnAngle);
+                                if (us.getDistance() < threshold){
+                                        travelForward(forwardThreshold);
+                                        claw.close();
+                                        break;
                                 }
-                                else {
-                                        travelForward(forwardDistance);
-                                        //then turn in the direction of the object
-                                        //turn around
-                                        theClaw.open();
-                                        theClaw.close();
-                                        stopSearch = true;
-                                }                               
-                }                    
+                        }
+                       
+                        rotateCCW(60);
+                        travelForward(forwardDistance);                                      
+                }
         }
-        
+       
         public void travelForward(double forwardDistance){
-                        leftMotor.setSpeed(FORWARD_SPEED);
-                        rightMotor.setSpeed(FORWARD_SPEED);
-                        leftMotor.rotate(convertDistance(leftRadius, forwardDistance), true);
-                        rightMotor.rotate(convertDistance(rightRadius, forwardDistance), false);
+                leftMotor.setSpeed(FORWARD_SPEED);
+                rightMotor.setSpeed(FORWARD_SPEED);
+                leftMotor.rotate(convertDistance(leftRadius, forwardDistance), true);
+                rightMotor.rotate(convertDistance(rightRadius, forwardDistance), false);
         }
-        
+       
         //rotate 45 degrees counterclockwise
         public void rotateCCW(double angle) {
             leftMotor.setSpeed(ROTATE_SPEED);
             rightMotor.setSpeed(ROTATE_SPEED);
             leftMotor.rotate(-convertAngle(leftRadius, width, angle), true);
             rightMotor.rotate(convertAngle(rightRadius, width, angle), false);
-            
+           
         }
+       
+        private int getFilteredData() {
+            int distance;
+            // do a ping
+            us.ping();
+            // wait for the ping to complete
+            try { Thread.sleep(50); } catch (InterruptedException e) {}
+            // there will be a delay here
+            distance = us.getDistance();
+            return distance;
+    }
        
        
         private static int convertDistance(double radius, double distance) {
@@ -85,16 +98,5 @@ public class blockPickUp<UltrasonicController> {
         private static int convertAngle(double radius, double width, double angle) {
                 return convertDistance(radius, Math.PI * width * angle / 360.0);
         }
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+ 
 }
